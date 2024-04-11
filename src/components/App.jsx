871f8b887f15c2b1c6df1ced46adc1423984c475
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-04-11 19:05:14 +0300

    firVer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import ImageModal from "./ImageModal/ImageModal";
+import Loader from "./Loader/Loader";
+import LoaderMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./SearchBar/SearchBar";
+import { requestImagesByQuery } from "./services/api";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [showBtn, setShowBtn] = useState(false);
+
+  useEffect(() => {
+    async function fetchImagesByQuery() {
+      try {
+        setLoading(true);
+        setError(false);
+        const { results, total_pages } = await requestImagesByQuery(query, page);
+        if (page > 1) {
+          setImages((prevImages) => [...prevImages, ...results]);
+        } else {
+          setImages(results);
+        }
+        setShowBtn(total_pages > page);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+      
+    }
+
+  }, []);
 
   return (
     <>
