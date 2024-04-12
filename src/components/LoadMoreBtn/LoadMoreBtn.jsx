ce5320dd32eefe70fc1edef255d73417e7e9f9bd
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-04-12 17:25:43 +0300

    AllworksSev

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,25 +1,18 @@
-import css from "./LoadMoreBtn.module.css";
 import { Link } from "react-scroll";
-
-// function LoadMoreBtn({loadMore}) {
-//     return (
-//         <div>
-//             <Link
-//              to="load-more-button"
-//       spy={true}
-//       smooth={true}
-//       offset={950}
-//       duration={2000}>
-//                 <button className={css.loadBtn} onClick={loadMore} id="load-more-button">Load more</button>
-//           </Link>
-//         </div>
-//     );
-// }
+import css from "./LoadMoreBtn.module.css";
 
 const LoadMore = ({ loadMore }) => {
   return (
     <div>
-      <button onClick={loadMore}>Load More</button>
+      <Link
+        to="load-more-button"
+        spy={true}
+        smooth={true}
+        offset={950}
+        duration={2000}
+      >
+        <button className={css.loadBtn} onClick={loadMore}>Load More</button>
+      </Link>
     </div>
   );
 };
