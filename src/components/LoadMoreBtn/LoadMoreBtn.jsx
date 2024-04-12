32fd52c844d0f1139e83296e9356d464c6f0cf86
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-04-12 13:31:22 +0300

    fourNoMod

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,19 @@
+import css from "./LoadMoreBtn.module.css";
+import { Link } from "";
+
+function LoadMoreBtn() {
+    return (
+        <div>
+            <Link
+             to="load-more-button"
+      spy={true}
+      smooth={true}
+      offset={950}
+      duration={2000}>      
+                <button className={css.loadBtn} onClick={loadMore} id="load-more-button">Load more</button>
+          </Link>
+        </div>
+    );
+}
+
+export default LoadMoreBtn;
